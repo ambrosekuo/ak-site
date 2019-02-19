5921150617cdcf80636b3e23a5ea4f219276184f
ambrose214 <ambrose.kuo@hotmail.com>
2019-02-19 14:22:30 -0500

    environment variables added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000);
+app.listen(port);
