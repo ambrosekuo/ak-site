81de57d843ba24b668be8ea72f48039552439eb4
ambrose214 <ambrose.kuo@hotmail.com>
2019-02-19 22:58:58 -0500

    pq setup

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+const result = require('dotenv').config();
 var express = require('express');
 var app = express();
+const { Client } = require('pg');
+var bodyParser = require('body-parser');
+
+
+//var connectionString = "postgres://*USERNAME*:*PASSWORD*@*HOST*" //delete if
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: !process.env.STAGE_SSL && true,
+});
+
+client.connect();
 
 var port = process.env.PORT || 3000;
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/public/index.html');
